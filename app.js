231bab17ae63cf00969758cd58c6d8ec2053af73
Lambdaxyzt <lambdaxyzt@gmail.com>
2023-05-02 04:05:42 +0430

    test

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 var proxy = require("node-tcp-proxy");
-var newProxy = proxy.createProxy(process.env.PORT, "127.0.0.1", 9090);
+ 
+var newProxy = proxy.createProxy(8080, "127.0.0.1" ,  9090, {
+    upstream: function(context, data) {
+        return data;
+    }
+});
